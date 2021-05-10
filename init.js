753229a05c76054f3417afe3f0a70c25714766be
Eric Oliver <olivere@futuredesigngroup.com>
2021-05-09 23:33:08 -0500

    added project files

---
+++ init.js
@@ -0,0 +1,18 @@
+const stars = 400
+
+for (let i =0; i < stars; i++) {
+    let star = document.createElement("div")
+    star.className = 'stars'
+    var xy = randomPosition();
+    star.style.top = xy[0] + 'px'
+    star.style.left = xy[1] + 'px'
+    document.body.append(star)
+}
+
+function randomPosition() {
+    var y = window.innerWidth
+    var x = window.innerHeight
+    var randomX = Math.floor(Math.random() * x)
+    var randomY = Math.floor(Math.random() * y)
+    return [randomX, randomY]
+}
